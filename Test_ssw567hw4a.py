7ea0f7888bffbb329676176e00329afabaa6de3b
BarlesCharkley75 <45805171+BarlesCharkley75@users.noreply.github.com>
2021-10-02 17:18:19 -0400

    Update Test_ssw567hw4a.py

--- Test_ssw567hw4a.py
+++ Test_ssw567hw4a.py
@@ -3,7 +3,7 @@ import ssw567hw4a
 
 class Test_ssw567hw4a(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(ssw567hw4a.getCommits('BarlesCharkley75'), 150)
+        self.assertEqual(ssw567hw4a.getCommits('BarlesCharkley75'), 153)
     def test_2(self):
         self.assertEqual(ssw567hw4a.getCommits(1234567),"User ID is not valid")
 if __name__ == '__main__':
